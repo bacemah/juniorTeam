7b187d76c68f055febf8344ebe4c4a97e083f800
Wecim Derwich <wassimderwich0@gmail.com>
2024-02-02 19:25:20 +0100

    mmm

--- script.js
+++ script.js
@@ -5,9 +5,42 @@ const sections = document.querySelectorAll(".section");
 const header = document.querySelector(".header");
 const buttons1 = document.querySelectorAll(".btn1");
 const test = document.querySelectorAll("div.category-btn");
+const navBar = document.querySelector(".navigator");
 let currentTab = 0;
 const maxTab = tabs.length;
-
+navBar.addEventListener("mouseover", function (e) {
+  if (e.target.classList.contains("nav_link")) {
+    const link = e.target;
+    const sibling = link.closest(".navigator").querySelectorAll(".nav_link");
+    const logo = link.closest(".navigator").querySelector("img");
+    sibling.forEach((el) => {
+      if (el != link) el.style.opacity = 0.5;
+    });
+    logo.style.opacity = 0.5;
+  }
+});
+navBar.addEventListener("mouseout", function (e) {
+  if (e.target.classList.contains("nav_link")) {
+    const link = e.target;
+    const sibling = link.closest(".navigator").querySelectorAll(".nav_link");
+    const logo = link.closest(".navigator").querySelector(".logo");
+    sibling.forEach((el) => {
+      if (el != link) el.style.opacity = 1;
+    });
+    logo.style.opacity = 1;
+  }
+});
+const navObs = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) navBar.classList.add("sticky");
+  else navBar.classList.remove("sticky");
+};
+const navObserver = new IntersectionObserver(navObs, {
+  root: null,
+  threshold: 0.15,
+  rootMargin: "17%",
+});
+navObserver.observe(header);
 const switchTab = function (nextTab) {
   tabs.forEach((t) => t.classList.remove("categories__tab--active"));
   tabsContent.forEach((c) => c.classList.remove("categories__content--active"));
@@ -27,9 +60,15 @@ const tabInterval = setInterval(() => {
 const sectionObs = function (entries, observer) {
   const [entry] = entries;
   if (!entry.isIntersecting) return;
+  /*if (entry.target.id === "section--3"){
+    entry.target.classList.remove("section--hidden");
+
+  }*/
   entry.target.classList.remove("section--hidden");
+  console.log(entry.childNodes);
   observer.unobserve(entry.target);
 };
+sectionObs(sections);
 const sectionObserver = new IntersectionObserver(sectionObs, {
   root: null,
   threshold: 0.15,
@@ -50,7 +89,8 @@ const btnObs = function (entries) {
 btnObs(buttons1);
 const buttonObserver = new IntersectionObserver(btnObs, {
   root: null,
-  threshold: 0.3,
+  threshold: 0.5,
+  rootMargin: "50%",
 });
 test.forEach(function (btn) {
   buttonObserver.observe(btn);
